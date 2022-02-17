767aaae75aa91e9dba2e8178ad4f6a53203f0f35
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2022-02-17 10:47:27 +0200

    Update _asroz.py

--- moussapro/pro/resources/assistant/_asroz.py
+++ moussapro/pro/resources/assistant/_asroz.py
@@ -1,1 +1,11 @@
+from userbot.Config import Config
 
+RBOT = Config.TG_BOT_USERNAME
+ROZA = Config.PRIVATE_GROUP_BOT_API_ID
+StJms = [
+    "▾∮ جـار اعـداد البـوت المسـاعد",
+    "▾∮ يـتم اكمـال اعـداد البـوت االمسـاعد",
+    "▾∮ تـم بنجاح اعـداد البـوت المسـاعد",
+]
+Stdec = "- 𝐏𝐑𝐎 𝖴𝖲𝖤𝖱𝖡𝖮𝖳 𝖠𝖲𝖲𝖨𝖳𝖠𝖭𝖳 \n- 𝐏𝐑𝐎 𝖥𝖮𝖱  {}\n- @moussa_pro "
+SetAbt = "- 𝐏𝐑𝐎 𝖴𝖲𝖤𝖱𝖡𝖮𝖳\n- 𝐏𝐑𝐎 𝖥𝖮𝖱  {}"
