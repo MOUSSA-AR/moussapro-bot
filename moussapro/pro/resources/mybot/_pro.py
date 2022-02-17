27d32a25098a41079a7e497aae34b1f7452b03af
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2022-02-17 10:57:34 +0200

    Update _pro.py

--- moussapro/pro/resources/mybot/_pro.py
+++ moussapro/pro/resources/mybot/_pro.py
@@ -1,1 +1,13 @@
+from userbot.Config import Config
+from userbot.plugins import mention
 
+RAZAN = Config.TG_BOT_USERNAME
+# for alive
+ROZ = (
+    f"**⌯︙بوت برو يعمل بنجاح 🤍،**\n"
+    f"**   - اصدار التليثون :** `1.23.0\n`"
+    f"**   - اصدار برو :** `4.0.0`\n"
+    f"**   - البوت المستخدم :** `{RAZAN}`\n"
+    f"**   - اصدار البايثون :** `3.9.6\n`"
+    f"**   - المستخدم :** {mention}\n"
+)
