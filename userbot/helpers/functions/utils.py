a699cdd3e5533cb175ea339326e7e1312458c366
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2022-02-20 22:44:46 +0200

    Update utils.py

--- userbot/helpers/functions/utils.py
+++ userbot/helpers/functions/utils.py
@@ -46,9 +46,9 @@ async def get_readable_time(seconds: int) -> str:
 # gban
 
 
-async def admin_groups(catub):
+async def admin_groups(moussabot):
     catgroups = []
-    async for dialog in catub.iter_dialogs():
+    async for dialog in moussabot.iter_dialogs():
         entity = dialog.entity
         if (
             isinstance(entity, Channel)
