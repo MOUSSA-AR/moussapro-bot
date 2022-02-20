fcdaa204c17dfdd49d96c2722af4a6faaaca9a75
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2022-02-21 01:15:59 +0200

    Update session.py

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -1,3 +1,11 @@
+# @moussa_pro - < https://t.me/moussa_pro >
+# Copyright (C) 2022 - MOUSSA-AR
+# All rights reserved.
+#
+# This file is a part of < https://github.com/MOUSSA-AR/moussabot >
+# Please read the GNU Affero General Public License in;
+# < https://github.com/MOUSSA-AR/moussapro-bot/blob/master/LICENSE
+# ===============================================================
 import sys
 
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
