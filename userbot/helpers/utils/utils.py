d6b745fe8f5fa9cd353b376b66f296979bf6b10a
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2022-02-20 21:35:55 +0200

    Update utils.py

--- userbot/helpers/utils/utils.py
+++ userbot/helpers/utils/utils.py
@@ -34,14 +34,14 @@ def run_async(loop, coro):
     return asyncio.run_coroutine_threadsafe(coro, loop).result()
 
 
-async def unsavegif(event, sandy):
+async def unsavegif(event, pro):
     try:
         await event.client(
             functions.messages.SaveGifRequest(
                 id=types.InputDocument(
-                    id=sandy.media.document.id,
-                    access_hash=sandy.media.document.access_hash,
-                    file_reference=sandy.media.document.file_reference,
+                    id=pro.media.document.id,
+                    access_hash=pro.media.document.access_hash,
+                    file_reference=pro.media.document.file_reference,
                 ),
                 unsave=True,
             )
