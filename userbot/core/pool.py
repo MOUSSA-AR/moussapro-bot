d83889f170debea7086bcc12a07dd7f681959652
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2022-02-21 13:05:54 +0200

    Update pool.py

--- userbot/core/pool.py
+++ userbot/core/pool.py
@@ -34,8 +34,8 @@ def _get() -> ThreadPoolExecutor:
 def _stop():
     _EXECUTOR.shutdown()
     # pylint: disable=protected-access
-    _LOG.info(_LOG_STR, f"Stopped Pool : {_EXECUTOR._max_workers} Workers")
+    _LOG.info(f"⏳ جار تحميل  : {_EXECUTOR._max_workers} ")
 
 
 # pylint: disable=protected-access
-_LOG.info(_LOG_STR, f"Started Pool : {_EXECUTOR._max_workers} Workers")
+_LOG.info(f"⌛ اكتمل التحميل  : {_EXECUTOR._max_workers} ")
